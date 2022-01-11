527fa9b208f02e543e070b197196123434eac728
[nimisharajeev123] <[nimisharajeev123@gmail.com]>
2022-01-11 16:03:42 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
+// import {CalendarComponent} from '@syncfusion/ej2-react-calendars'
 import logo from './logo.svg';
 import './App.css';
+// import { ScheduleComponent } from '@syncfusion/ej2-react-schedule';
+import { Inject ,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda,EventSettingsModel} from '@syncfusion/ej2-react-schedule'
+import { DataManager,WebApiAdaptor } from '@syncfusion/ej2-data';
+  class App extends React.Component{
+  private localData:EventSettingsModel ={
+   
+    dataSource : [{
+      EndTime: new Date(2019,0,11,6,30),
+      StartTime:new Date(2019,0,11,4,0)
+    }]
+  };
+  private remoteData = new DataManager({
+    url:'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+    adaptor: new WebApiAdaptor,
+    crossDomain:true
+  });
+  public render(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+      
+  
+  
+  return <ScheduleComponent currentView='Month' selectedDate={ new Date (2017,5,5)}
+        eventSettings={ { dataSource: this.remoteData}}>
+        <Inject services={[Day,Week,WorkWeek,Month,Agenda]} /> </ScheduleComponent>
+        
+
+  
 }
+  }
+
 
 export default App;
